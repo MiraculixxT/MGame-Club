02969873a3b6f4ac1b0e260317679eb2dd524bc8
Miraculixx <miraculixxyt@gmail.com>
2022-11-01 20:50:52 +0100

    Trivia - Replay fix

--- src/main/kotlin/de/miraculixx/mcord/modules/trivia/TriviaCommand.kt
+++ src/main/kotlin/de/miraculixx/mcord/modules/trivia/TriviaCommand.kt
@@ -64,7 +64,7 @@ suspend fun generateQuestion(category: TriviaCategory, difficulty: TriviaDifficu
         val cat = TriviaCategory.getByTitle(trivia.category.replace(":", " -"))
         title = "\uD83E\uDDE9  || **Trivia Quiz**"
         description = "> Difficulty -> ``${diff?.title}``\n" +
-                "> Category -> ``${cat.title}``\n" +
+                "> Category -> ``${cat.title}${if (category == TriviaCategory.RANDOM) " (random)" else ""}``\n" +
                 "\n" +
                 "```fix\n" +
                 "${question}```"
