2c036f8352b201004e9ab4fe31a12555447a46b7
miraculixx <miraculixxyt@gmail.com>
2026-06-09 14:44:18 +0200

    Add centralized icons

--- src/main/kotlin/de/miraculixx/mgames/utils/CachingData.kt
+++ src/main/kotlin/de/miraculixx/mgames/utils/CachingData.kt
@@ -1,9 +1,4 @@
 package de.miraculixx.mgames.utils
 
-import net.dv8tion.jda.api.entities.emoji.Emoji
-
 var cachedDailySeed: Long? = null
 var cachedDailyDate: String? = null
-
-val mCoin = "<:mcoin:996386525208117258>"
-val mCoinEmoji = Emoji.fromCustom("mcoin", 996386525208117258, false)
---
+++ src/main/kotlin/de/miraculixx/mgames/utils/Icons.kt
@@ -0,0 +1,15 @@
+package de.miraculixx.mgames.utils
+
+object Icons {
+    // General things
+    val mCoins = "<:mcoin:996386525208117258>"
+    val blanc = "<:blanc:1059482494644269056>"
+
+    // Game generic
+    val x = "<:xx:988156472020066324>"
+    val o = "<:oo:988156473274163200>"
+
+    // Game specific
+    val tictactoe = "<:tictactoe:1513850555796029531>"
+
+}
